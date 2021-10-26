d48eb109871551a0ce83dd1e422ed6b4acffe9c5
Angelo Pellegrino <angelo.pellegrino@it.abb.com>
2021-10-26 21:50:56 +0200

    ancora perde la connessione dopo un po

--- THERMOPOT.ino
+++ THERMOPOT.ino
@@ -26,7 +26,7 @@ Mettere manopola a 24 gradi
  Il trick è mettere una resistenza in parallelo a R12 e staccarla grazie al rele
 
 
- Collegare resistenza fra 1 e D del rele
+
 
  Quando il gpio è LOW-> corto CD ->Req = R12
  Quando GPIO è HIGH -> cort DL -> Req = parallelo R12 e R1k -> 12gradi
@@ -41,14 +41,20 @@ Mettere manopola a 24 gradi
 #define CURSOR_X0 50
 #define CURSOR_Y0 50
 
+#define SCREEN_HEIGHT 135
+#define SCREEN_WIDTH  240
+
 
 #define RISCA_ON  LOW
 #define RISCA_OFF HIGH
 
 #define RELEPIN  22
 
+
+#define TFT_WALL_BLUE 0x1947
+
 // Use hardware SPI
-TFT_eSPI tft = TFT_eSPI(200,320);
+TFT_eSPI tft = TFT_eSPI(SCREEN_HEIGHT,SCREEN_WIDTH);
 AsyncUDP udp;
 
 struct Thermo_T
@@ -63,12 +69,12 @@ struct Thermo_T
 
 void SetCursor(int x, int y)
 {
-    tft.setCursor(CURSOR_X0+x, CURSOR_Y0 + y, 2);
+    tft.setCursor(0+x, 0 + y, 2);
 }
 
 void DrawRect(unsigned short  x, unsigned short  y, unsigned short  w, unsigned short l, unsigned short colo)
 {
-     tft.fillRect(x + CURSOR_X0, y + CURSOR_Y0, w, l, colo);
+     tft.fillRect(x + 0, y + 0, w, l, colo);
 }
 
 unsigned long drawTime = 0;
@@ -130,33 +136,54 @@ const char* topics[MAX_TOPICS] = {topic_t1,topic_t2,topic_tExt,topic_setpoint,to
 
 static bool state = false;
 
+bool toggle_info = false;
 
 
-const byte red = 38;
-const byte green = 60;
-const byte blue = 117;
 
-byte color_blue[3] = {red,green,blue};
+
 byte color_orange[3] = {(byte)232,(byte)157,(byte)19};
-byte* color = (byte *)&color_blue;
+
 
 ///////////////////////////timers//////////////////////////////
 static int timer = 0;
 static int mils = 0;
 
+static int timergui = 0;
+static int milsgui = 0;
+
 void TimerCharge(int seconds)
 {
-  static int timer = seconds*1000;
-  static int mils = millis();
+   mils = millis()+ seconds*1000;
+
+}
+
+void TimerGUICharge(int seconds)
+{
+    
+   milsgui = millis()+ seconds*1000;
 }
 
 bool TimerCheck()
 {
-  int decs = millis()-mils;
-  timer-= decs;
-
-  if(timer == 0)
+  int decs = 0;
+  if (millis() >= mils )
   {
+    mils = 0;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+bool TimerGUICheck()
+{
+  int decs = 0;
+  if (millis() >= milsgui )
+  {
+    milsgui = 0;
     return true;
   }
   else
@@ -228,7 +255,7 @@ void callback(char* topic, byte* payload, unsigned int length)
   }
   
 
-  GUI_DRAW();
+  GUI_DRAW(false);
    
   
 }
@@ -279,7 +306,7 @@ bool Thermo_Logic()
 
 
 
-void GUI_DRAW()
+void GUI_DRAW(bool info)
 {
   /**
     Salotto Soffitta
@@ -289,22 +316,34 @@ void GUI_DRAW()
   
   */
 
-  tft.setTextColor(TFT_WHITE,(color[0],color[1],color[2])); 
+  tft.setTextColor(TFT_WHITE,TFT_WALL_BLUE); 
 
-  tft.fillScreen((color[0],color[1],color[2]));
+  tft.fillScreen(TFT_WALL_BLUE);
   SetCursor(0,0);
   tft.setTextSize(3);
-  tft.print(  Thermo.t1 ,1  );
+  if(false == info)
+  {
+    tft.print(  Thermo.t1 ,1  );
+  }
+  else
+  {
+    tft.print( "SAL" );
+  }
   
-  SetCursor(120,0);
+  SetCursor(150,0);
   tft.setTextSize(3);
-  tft.print(   Thermo.t2  ,1 );
+  if(false == info)
+  {
+    tft.print(  Thermo.t2 ,1  );
+  }
+  else
+  {
+    tft.print( "SOF" );
+  }
 
   SetCursor(80,80);
   tft.setTextSize(2);
-  tft.setTextColor(TFT_CYAN,(color[0],color[1],color[2])); 
-             
-
+  tft.setTextColor(TFT_CYAN,TFT_WALL_BLUE); 
   tft.print(   Thermo.set_point_c , 1   );
 
   if(Thermo.presence == 4)
@@ -315,17 +354,17 @@ void GUI_DRAW()
   {
     if(Thermo.presence & 1) //salotto
     {
-      DrawRect(0, 50, 80, 10, TFT_WHITE );
+      DrawRect(0, 50, 80, 5, TFT_WHITE );
     }
     if(Thermo.presence & 2) //soffitta
     {
-       DrawRect(120, 50, 80, 10, TFT_WHITE );
+       DrawRect(120, 50, 80, 5, TFT_WHITE );
     }
   }
 
   if(true == Thermo_Logic())
   {
-    DrawRect(20, 60, 200, 10, TFT_ORANGE );
+    DrawRect(20, 63, SCREEN_WIDTH-40, 10, TFT_ORANGE );
   }
 
   //MQTT STATUS...but if i am here it works...
@@ -339,14 +378,15 @@ void GUI_DRAW()
 //////////////////////////////////////////////////////////////
 
 
-
+#define INIT_CON             0
+#define TE_SCREEN_INIT_DRAW 1
 
 
 void loop()
 {
   switch(task)
   {
-    case 0:
+    case INIT_CON :
        WiFi.begin(ssid, password);
  
       while (WiFi.status() != WL_CONNECTED)
@@ -365,7 +405,7 @@ void loop()
         if (client.connect("ESP32Client", mqttUser, mqttPassword )) 
         {
           Serial.println("connected");  
-          task = 1;
+          task = TE_SCREEN_INIT_DRAW;
           //client.subscribe(topic_t1);
           //client.subscribe(topic_setpoint);
 
@@ -385,11 +425,10 @@ void loop()
       
     break;
 
-    case 1:
-        tft.fillScreen((color[0],color[1],color[2]));
+    case TE_SCREEN_INIT_DRAW:
+        tft.fillScreen(TFT_WALL_BLUE);
         SetCursor(0,0);
         // Set the font colour to be white with a black background, set text size multiplier to 1
-        tft.setTextColor(TFT_WHITE,(color[0],color[1],color[2]));  
         tft.setTextSize(2);
         // We can now plot text on screen using the "print" class
         //tft.print("MQTT in ");
@@ -398,6 +437,7 @@ void loop()
   DrawRect(0, 130, 40, 10, TFT_GREEN );
        // tft.fillRect(, y + CURSOR_Y0, w, l, colo);
         TimerCharge(5);
+         TimerGUICharge(4);
         task = 3;
     break;
 
@@ -405,17 +445,32 @@ void loop()
 
     case 3:
       client.loop();
-      if(TimerCheck())
+      if(true == TimerGUICheck())
+      {
+         if(toggle_info == true)
+         {
+           GUI_DRAW(true);
+          toggle_info = false;
+          TimerGUICharge(1);
+         }
+         else
+         {
+          GUI_DRAW(false);
+          toggle_info = true;
+          TimerGUICharge(4);
+         }
+      }
+      
+      if(true == TimerCheck())
       {
         task = 4;
       }
-      
     break;
 
     case 4: //check conn 
       if(WiFi.status() != WL_CONNECTED)
       {
-        task = 0;
+        task = INIT_CON;
         DrawRect(0, 120, 40, 20, TFT_BLACK);
 
       }
